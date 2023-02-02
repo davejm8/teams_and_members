249fe0c5ecf3a2f155979a883dfbb8c8b5246247
David Marino <davidjmarino8@gmail.com>
2023-02-02 15:45:29 -0500

    User story 7 complete

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,7 @@
 class Team < ApplicationRecord
   has_many :members
+
+  def member_count
+    self.members.count
+  end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -4,4 +4,20 @@ describe Team do
   describe 'relations' do
     it { should have_many :members }
   end
+
+  describe 'instance methods' do
+    before :each do
+      @smash = Team.create!(name: "Super Smash Bros", games_won: 12, won_championship: true)
+      @mario = Member.create!(name: "Mario", wage: 95.05, injured: false, team_id: @smash.id)
+      @luigi =  Member.create!(name: "Luigi", wage: 90.00, injured: true, team_id: @smash.id)
+    end
+
+    describe '#member_count' do
+      it 'counts the number of members on a team' do
+        expect(@smash.member_count).to eq(2) 
+      end
+    end
+    
+  end
+
 end
